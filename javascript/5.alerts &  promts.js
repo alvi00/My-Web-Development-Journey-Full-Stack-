8272f2e0a1442fccf0bead272c2bc4e7a22cf17e
Ahmad Fahmid <ahmadfahmid59@gmail.com>
2024-03-16 00:50:12 +0600

    Create 5.alerts &  promts.js

---
+++ javascript/5.alerts &  promts.js
@@ -0,0 +1,17 @@
+// Printing a warning message to the console
+console.warn("This is a warning");
+
+// Printing an error message to the console
+console.error("This is an error");
+
+// Asking the user to input their first name through a prompt dialog
+let firstname = prompt("Enter your first name");
+
+// Asking the user to input their last name through a prompt dialog
+let finalname = prompt("Enter your last name");
+
+// Concatenating the first name and last name input by the user
+let msg = (firstname + " " + finalname);
+
+// Displaying a welcome message along with the concatenated full name
+alert("Welcome " + msg);
