1544df4b23e3b39d89b512fc1b72272ea9f29e68
Ahmad Fahmid <ahmadfahmid59@gmail.com>
2024-04-06 11:41:57 +0600

    Create 6.string_methord.js

---
+++ javascript/6.string_methord.js
@@ -0,0 +1,8 @@
+// Prompt the user to enter their password and store the input in the variable 'password'
+let password = prompt("Enter your password");
+
+// Trim the whitespace from the beginning and end of the password using the trim() method
+let alvi = password.trim();
+//here it doesnt make change to the main str. it make a copy of the str, make changes and give the new string.
+// Log the trimmed password to the console
+console.log(alvi);
