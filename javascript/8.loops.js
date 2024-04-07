2abdec58d2daccaac64d9d160972588eb9d8a770
Ahmad Fahmid <ahmadfahmid59@gmail.com>
2024-04-07 14:11:34 +0600

    Create 8.loops.js

---
+++ javascript/8.loops.js
@@ -0,0 +1,10 @@
+//----------------------------------------------------------------------
+
+// Iterate over numbers from 1 to 15
+for (let i = 1; i <= 15; i++) {
+    // Check if the number is odd by checking if the remainder of division by 2 is not equal to 0
+    if (i % 2 != 0) {
+        // If the number is odd, log it to the console
+        console.log(i);
+    }
+}
