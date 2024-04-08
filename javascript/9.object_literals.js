fac881788c452b04b8ed651e5aebd6e7378fa37a
Ahmad Fahmid <ahmadfahmid59@gmail.com>
2024-04-08 16:24:12 +0600

    Create 9.object_literals.js

---
+++ javascript/9.object_literals.js
@@ -0,0 +1,26 @@
+//----------------------------------------------------------------------
+
+// Define a constant object 'a' with properties 'name', 'roll', and 'mark'
+const a = {
+    name: "alvi",
+    roll: "123",
+    mark: 12
+};
+
+// Log the contents of object 'a' to the console
+console.log(a);
+
+// Define a variable 'item' with properties 'name', 'price', and 'color'
+let item = {
+    name: "pencil",
+    price: 10,
+    color: ["red", "pink"]
+};
+
+// Log the contents of object 'item' to the console
+console.log(item);
+
+// Log the value of the 'price' property of object 'item' to the console
+console.log(item.price);
+
+//----------------------------------------------------------------------
