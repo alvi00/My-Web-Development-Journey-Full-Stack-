c6032f4b0317a3f97082adee914f253d59350a7e
Ahmad Fahmid <ahmadfahmid59@gmail.com>
2024-04-09 14:30:53 +0600

    Create 10.funcstions.js

---
+++ javascript/10.funcstions.js
@@ -0,0 +1,15 @@
+//----------------------------------------------------------------------
+
+// Define a function 'malti' that takes a parameter 'a'
+function malti(a) {
+    // Iterate from 1 to 10
+    for (let i = 1; i <= 10; i++) {
+        // Multiply 'a' by the current iteration value 'i' and print the result
+        console.log(`${a} * ${i} = ${a * i}`);
+    }
+}
+
+// Call the 'malti' function with an argument of 5
+malti(5);
+
+//----------------------------------------------------------------------
