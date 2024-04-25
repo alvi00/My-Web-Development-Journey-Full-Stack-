aaf8443c202e7bb204200bfe3723a785b0d9fda6
Ahmad Fahmid <ahmadfahmid59@gmail.com>
2024-04-26 02:14:28 +0600

    Update 11.this, arrow, try-catch.js

--- javascript/11.this, arrow, try-catch.js
+++ javascript/11.this, arrow, try-catch.js
@@ -7,10 +7,24 @@ let alvi = {
 
     // Method to calculate average score
     avg: function() {
-        // Calculate the sum of scores and divide by the number of subjects (3)
-        console.log((this.eng + this.math + this.phy) / 3); // Output the average score
+        // Output 'this', referring to the current object 'alvi'
+        console.log(this);
+
+        // Calculate the sum of scores and attempt to output average score
+        // The division operation is mistakenly placed outside the console.log() function
+        // This will cause only the sum to be logged, and the division result will be printed to the console separately
+        console.log(this.eng + this.math + this.phy) / 3;
     }
 };
 
 // Call the 'avg' method of the 'alvi' object to calculate and display the average score
 alvi.avg();
+
+// Define a function named 'anas'
+function anas() {
+    // Output 'this', referring to the global object (in a browser environment, it would refer to the 'window' object)
+    console.log(this);
+}
+
+// Call the 'anas' function
+anas();
