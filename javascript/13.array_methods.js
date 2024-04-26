e489bd41fd7639164f82e56ec0b9c8b20cb092d2
Ahmad Fahmid <ahmadfahmid59@gmail.com>
2024-04-26 23:21:45 +0600

    Update 13.array_methods.js

--- javascript/13.array_methods.js
+++ javascript/13.array_methods.js
@@ -1,3 +1,32 @@
 //--------------------------------------/
+  //for each
+//-------------------------------------//
+
+// Define an array containing integers from 1 to 8
+let arr = [1, 2, 3, 4, 5, 6, 7, 8];
+
+// Define a function named 'alvi' using arrow function syntax
+// This function takes a parameter 'el' and logs it to the console
+let alvi = (el) => {
+    console.log(el);
+};
+
+// Iterate over each element of the array 'arr' using the 'forEach' method
+// For each element, call the 'alvi' function, which logs the element to the console
+arr.forEach(alvi);
+
+// Log "Second way" to the console, separating the output from the previous 'forEach' call
+console.log("Second way");
+
+// Alternative approach: Iterate over each element of the array 'arr' using 'forEach'
+// Instead of passing the 'alvi' function directly, define an anonymous function inside 'forEach'
+// This anonymous function accomplishes the same task as 'alvi' - logging each element to the console
+arr.forEach(function(el) {
+    console.log(el);
+});
+
+
+
+//--------------------------------------/
 
 //-------------------------------------//
