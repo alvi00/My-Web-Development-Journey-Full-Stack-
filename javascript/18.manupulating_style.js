7e6a6bfbc9d0a5ffb0c647f1adfa87943396d212
Ahmad Fahmid <ahmadfahmid59@gmail.com>
2024-04-29 03:38:49 +0600

    Create 18.manupulating_style.js

---
+++ javascript/18.manupulating_style.js
@@ -0,0 +1,11 @@
+// Select the first <h1> element in the document.
+let alvi = document.querySelector("h1");
+
+// Set the color of the text within the <h1> element to green.
+alvi.style.color = "green";
+
+// Set the cursor style to "help" when hovering over the <h1> element.
+alvi.style.cursor = "help";
+
+// Set the text decoration of the <h1> element to underline.
+alvi.style.textDecoration = "underline";
