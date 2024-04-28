f0c75dcef568d8056c890bbbf81479e0b00b4e1a
Ahmad Fahmid <ahmadfahmid59@gmail.com>
2024-04-29 01:37:15 +0600

    Create 15.Dom_obejcts_query_selector.js

---
+++ javascript/15.Dom_obejcts_query_selector.js
@@ -0,0 +1,31 @@
+
+//in this case it will scan the whole code and it will select the first occurnce of h1
+//in always passes an object
+
+
+// Select the first occurrence of <h1> element in the DOM.
+let alvi = document.querySelector("h1");
+
+// Log the selected <h1> element to the console.
+console.log(alvi);
+
+// Log the properties and methods of the selected <h1> element to the console in a detailed way.
+console.dir(alvi);
+
+// Select the element with the ID "mainimg" in the DOM.
+let anas = document.querySelector("#mainimg");
+
+// Log the selected element to the console.
+console.log(anas);
+
+// Log the properties and methods of the selected element to the console in a detailed way.
+console.dir(alvi);
+
+// Select the first element with the class "oldimg" in the DOM.
+let sakib = document.querySelector(".oldimg");
+
+// Log the selected element to the console.
+console.log(sakib);
+
+// Log the properties and methods of the selected element to the console in a detailed way.
+console.dir(alvi);
