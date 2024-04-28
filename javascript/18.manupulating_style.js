4a2c47c092dca223948de27624e0098572390af9
Ahmad Fahmid <ahmadfahmid59@gmail.com>
2024-04-29 03:45:15 +0600

    Update 18.manupulating_style.js

--- javascript/18.manupulating_style.js
+++ javascript/18.manupulating_style.js
@@ -9,3 +9,13 @@ alvi.style.cursor = "help";
 
 // Set the text decoration of the <h1> element to underline.
 alvi.style.textDecoration = "underline";
+
+// Select all <a> elements with the class "pubInfo" in the document.
+let alvi = document.querySelectorAll(".pubInfo a");
+// Loop through each <a> element in the collection.
+for (let i = 0; i < alvi.length; i++) {
+    // Set the text decoration of each <a> element to "none", removing any underline.
+    alvi[i].style.textDecoration = "none";
+    // Set the color of each <a> element to red.
+    alvi[i].style.color = "red";
+}
