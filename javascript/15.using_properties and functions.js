8878faf45e1cd0e74e80067b94e38d5bae4e63e1
Ahmad Fahmid <ahmadfahmid59@gmail.com>
2024-04-29 02:26:59 +0600

    Create 15.using_properties and functions.js

---
+++ javascript/15.using_properties and functions.js
@@ -0,0 +1,8 @@
+// Select the first <p> element in the document.
+let alvi=document.querySelector("p");
+console.log(alvi.innerHTML);
+//shows text of the webpage with tags like bold tag or anchor tag
+console.log(alvi.innerText);
+//shows the text that is visible in the webpage
+console.log(alvi.textContent);
+//shows the text how we wrote the text in the index.html file in vs code
