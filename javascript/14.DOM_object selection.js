412500c303aacb56bafe85041497cdf6034627dd
Ahmad Fahmid <ahmadfahmid59@gmail.com>
2024-04-28 20:58:08 +0600

    Update 14.DOM_object selection.js

--- javascript/14.DOM_object selection.js
+++ javascript/14.DOM_object selection.js
@@ -1,6 +1,21 @@
 // DOM (Document Object Model) represents the structure of an HTML document as a tree of objects.
+//`````````````````````````````````//
+//Select elements by ID
+//`````````````````````````````````//
+
+
 // In the following code snippet, we first locate an HTML element with the ID "mainimg" in the DOM.
 let imgobj=document.getElementById("mainimg");
 //when we select by id then it returns an Object
 // Then, we set the 'src' attribute of the found element to "Assets/Man.png".
 imgobj.src="Assets/Man.png";
+
+
+//`````````````````````````````````//
+//Select elements by classname
+//`````````````````````````````````//
+
+//when selecting an element by classname it returns an collection
+
+
+
