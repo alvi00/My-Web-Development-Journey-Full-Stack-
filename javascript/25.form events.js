3fb26bcac5ddc6a0e295ed42f867287d955223f1
Ahmad Fahmid <ahmadfahmid59@gmail.com>
2024-04-29 20:17:25 +0600

    Update 25.form events.js

--- javascript/25.form events.js
+++ javascript/25.form events.js
@@ -1,1 +1,26 @@
 /````````````````````````//
+//event listerners for form
+  //learn more from mdn
+  //`````````````````````````//
+  // Select the <form> element in the document and assign it to the variable alvi
+let alvi = document.querySelector("form");
+
+// Add an event listener to the <form> element for the "submit" event,
+// which will call a function when the form is submitted
+alvi.addEventListener("submit", function(event) {
+    // Log a message to the console indicating that the form is submitted
+    console.log("The form is submitted");
+    // Log the event object to the console, displaying its properties
+    console.dir(event);
+});
+
+// Add another event listener to the <form> element for the "submit" event,
+// which will call a function when the form is submitted
+alvi.addEventListener("submit", function(event) {
+    // Prevent the default form submission behavior
+    event.preventDefault();
+    // Log a message to the console indicating that the form is submitted
+    console.log("The form is submitted");
+    // Log the event object to the console, displaying its properties
+    console.dir(event);
+});
