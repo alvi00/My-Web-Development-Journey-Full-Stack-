93815e91ce28299747cd60c4fc5be49586be0d7b
Ahmad Fahmid <ahmadfahmid59@gmail.com>
2024-04-29 14:34:03 +0600

    Create 22.Dom Events.js

---
+++ javascript/22.Dom Events.js
@@ -0,0 +1,39 @@
+//``````````````````````````````````//
+//This is inline way for onclick
+//But is is not recommended
+//```````````````````````````````````//
+
+<!-- Start of HTML document -->
+<html lang="en">
+<head>
+    <!-- Meta tag specifying character set -->
+    <meta charset="UTF-8">
+    <!-- Meta tag specifying viewport settings for responsive design -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Title of the webpage -->
+    <title>DOM Manipulation</title>
+    <!-- Link to an external CSS file for styling -->
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <!-- Start of body content -->
+    
+    <!-- Button element with an onclick attribute triggering two console.log messages -->
+    <button onclick="console.log('Button is clicked'); console.log('Once')" >Click Me!</button>
+    
+    <!-- Script tag linking to an external JavaScript file -->
+    <script src="app.js"></script>
+    
+    <!-- End of body content -->
+</body>
+<!-- End of HTML document -->
+</html>
+
+
+
+//``````````````````````````````````//
+//This is inline way for onclick
+//But is is not recommended
+//```````````````````````````````````//
+
+
