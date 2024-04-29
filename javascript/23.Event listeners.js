d3d5c2e5b185eb24c7180c06b9485041d2480e01
Ahmad Fahmid <ahmadfahmid59@gmail.com>
2024-04-29 17:28:13 +0600

    Update 23.Event listeners.js

--- javascript/23.Event listeners.js
+++ javascript/23.Event listeners.js
@@ -24,3 +24,26 @@ btn.addEventListener("click", sayhello);
 btn.addEventListener("mouseenter", hru);
 
 
+//```````````````````````````````//
+//adding p as a event listener
+//```````````````````````````````//
+// Select the <p> element in the document
+let para = document.querySelector("p");
+
+// Define a function named alvi that sets the text of the <p> element to "I am clicked"
+let alvi = () => {
+    para.innerText = "I am clicked";
+};
+
+// Define a function named anas that sets the text of the <p> element to "THIS IS A PARA"
+let anas = () => {
+    para.innerText = "THIS IS A PARA";
+};
+
+// Add an event listener to the <p> element for the "mouseenter" event,
+// which will call the alvi function when the mouse enters the <p> element
+para.addEventListener("mouseenter", alvi);
+
+// Add an event listener to the <p> element for the "mouseleave" event,
+// which will call the anas function when the mouse leaves the <p> element
+para.addEventListener("mouseleave", anas);
