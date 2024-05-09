d27eae31b9217a0d79d6dde8e532f6f52fead811
Ahmad Fahmid <ahmadfahmid59@gmail.com>
2024-05-09 14:12:30 +0600

    Update 9_29.call_back_hell.js

--- javascript/9_29.call_back_hell.js
+++ javascript/9_29.call_back_hell.js
@@ -28,3 +28,43 @@ let button = document.querySelector("button");
 
 // Set the inner text of the button to "alvi"
 button.innerText = "alvi"; 
+
+
+
+
+//````````````````````````````````````````//
+
+//Another example
+//````````````````````````````````````````````//
+
+// Define a function named savetodb that takes three parameters: data, success callback function, and failure callback function
+function savetodb(data, success, failure) {
+    // Generate a random number between 1 and 10 to simulate the insertion speed
+    let intertspeed = Math.floor(Math.random() * 10) + 1;
+    // If the insertion speed is greater than 4, call the success callback function
+    if (intertspeed > 4) {
+        success();
+    } else {
+        // If the insertion speed is 4 or less, call the failure callback function
+        failure();
+    }
+}
+
+// Call the savetodb function with the data "Alvi is a good boy"
+savetodb("Alvi is a good boy", () => {
+    // Success callback function: Log a success message to the console when the data is saved
+    console.log("Your data is saved");
+    // Call the savetodb function again with another data
+    savetodb("This is second data", () => {
+        // Success callback function: Log a success message to the console when the second data is saved
+        console.log("Your data is saved again");
+    }, () => {
+        // Failure callback function: Log a failure message to the console if the second data could not be saved
+        console.log("Your data could not be saved again");
+    });
+}, () => {
+    // Failure callback function: Log a failure message to the console if the first data could not be saved
+    console.log("Your data could not be saved");
+});
+
+
