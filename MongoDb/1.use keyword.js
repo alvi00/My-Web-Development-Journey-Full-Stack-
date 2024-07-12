64cfa499490794960d5224c34c1ac17fa46ec6ef
Ahmad Fahmid <ahmadfahmid59@gmail.com>
2024-07-12 11:26:22 +0600

    Add files via upload

---
+++ MongoDb/1.use keyword.js
@@ -0,0 +1,19 @@
+//go to cmd
+//write mongosh
+
+
+
+
+//to create a database or to use a database write use then the name of your database
+//so if there is no database and you write use college
+//then it will make a temporary database called college
+// it will remain temporary untill i insert any data into the database
+
+use college;
+
+//to know the current database write db
+db
+
+//to know all the created databases write dbs
+
+dbs
